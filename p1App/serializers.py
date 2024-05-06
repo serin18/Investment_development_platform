0b59b4b445fec632d2aa0c219848c14dcea29087
serin_18 <serinanna5@gmail.com>
2024-05-06 13:04:28 +0530

    kk

--- p1App/serializers.py
+++ p1App/serializers.py
@@ -4,5 +4,6 @@ from p1App.models import *
 class InventorRegSerial(serializers.ModelSerializer):
     class Meta:
         model=CustomUserdb
-        fields=['username','full_name','mobile','country','designation','proff_bio','twitter','linkedin','']
+        fields=['full_name','mobile','country','designation','proff_bio','twitter','linkedin','is_innovator']
+
 
